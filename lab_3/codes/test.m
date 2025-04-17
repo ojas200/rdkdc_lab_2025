709ecd2c8fa65418e8431a9f9cf188d34b262114
ojas2412 <128888678+ojas2412@users.noreply.github.com>
2025-04-17 16:11:30 -0400

    added manipulability

--- lab_3/codes/test.m
+++ lab_3/codes/test.m
@@ -1,11 +1,6 @@
 clear all;
 close all;
 
-g_test = randSE3();
-disp(g_test);
-c_hat = logm(g_test);
-disp(c_hat);
-v = c_hat(1:3,4);
-w = [-c_hat(2,3),c_hat(1,3),-c_hat(1,2)]';
-twist = [v;w];
-disp(twist);
+t = eye(6,6);
+A = det(t);
+disp(A);
