45e8980612c1fc60abb1c36c00ea6b8f33fadfa5
ojas2412 <128888678+ojas2412@users.noreply.github.com>
2025-04-18 18:42:19 -0400

    changed folder names

--- codes/manipulability.m
+++ codes/manipulability.m
@@ -2,11 +2,27 @@
 %or invcond as defined in Chapter 3, 4.4, MLS. Return manipulability
 %measure 
 function mu = manipulability(J,measure)
-    if measure == 'sigmamin'
-    
-    elseif measure == 'detjac'
-       
-    else
+    if strcmp(measure,'sigmamin')
+        %Return minimum singular value of J. If singular, return 0
+        A = svd(J); %Retuns singular values in descending order
+        mu = A(end); %Gets minimum element
+        if mu < 1e-5 %Checking for minuscule element and returning zero
+            mu = 0;
+        end
 
+    elseif strcmp(measure,'invcond')
+        %Return ratio of min and max singular values
+        B = svd(J);
+        mu = B(end)/B(1);
+        if mu < 1e-5 %Checking for minuscule element and returning zero
+            mu = 0;
+        end
+        
+    else
+        %Detjac. Return determinant of matrix
+        mu = det(J);
+        if mu < 1e-5
+            mu = 0;
+        end
     end
 end
--- lab_3/codes/getXi.m
+++
@@ -1,8 +0,0 @@
-%Takes in homogeneous transformation matrix as input. Outputs unnormalized
-%twist vector c_hat_prime such that g = exp(c_hat_prime)
-function xi = getXi(g)
-    c_hat = logm(g); %Matrix logarithm returns skew symmetric unnormalized form
-    v = c_hat(1:3,4);
-    w = [-c_hat(2,3),c_hat(1,3),-c_hat(1,2)]';
-    xi = [v;w]; 
-end
